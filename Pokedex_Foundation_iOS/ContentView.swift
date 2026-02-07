065d19fbe9a1300177ae7061d411118f1eea2c57
Pedro Toma <pedroakira.toma@gmail.com>
2026-02-07 19:25:51 -0300

    Initial Commit

---
+++ Pokedex_Foundation_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokedex_Foundation_iOS
+//
+//  Created by Pedro Akira Cardoso Toma on 07/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokedex_Foundation_iOS/Pokedex_Foundation_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_Foundation_iOSApp.swift
+//  Pokedex_Foundation_iOS
+//
+//  Created by Pedro Akira Cardoso Toma on 07/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_Foundation_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
